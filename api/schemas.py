b44117dcbe6543bdd3ea77d7df5c9ac335b1086f
Abdelhalim addad <abdelhalim.resu@gmail.com>
2018-10-21 22:06:03 +0100

    Make offer fields required

--- api/schemas.py
+++ api/schemas.py
@@ -1,16 +1,15 @@
 from marshmallow import fields, Schema
 
-
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
-    username = fields.Str()
-    password = fields.Str()
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
 
 
 class OfferSchema(Schema):
     id = fields.Int(dump_only=True)
-    title = fields.Str()
-    description = fields.Str()
-    skills_list = fields.List(fields.String)
+    title = fields.Str(required=True)
+    description = fields.Str(required=True)
+    skills_list = fields.List(fields.String, required=True)
     creation_date = fields.DateTime(dump_only=True)
     modification_date = fields.DateTime(dump_only=True)
